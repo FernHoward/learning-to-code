d7f0d9c77c5412d22aa3485ddf0401581023199d
Fern Howard <fern.b.howard@gmail.com>
2017-06-08 16:58:37 -0400

    added items to list

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var itemTemplate = $('#templates .item')
 var list         = $('#list')
+
 var addItemToPage = function(itemData) {
   var item = itemTemplate.clone()
   item.attr('data-id',itemData.id)
@@ -9,14 +10,29 @@ var addItemToPage = function(itemData) {
   }
   list.append(item)
 }
+
 var loadRequest = $.ajax({
   type: 'GET',
   url: "https://listalous.herokuapp.com/lists/Wash-Day-Routine-Checklist/"
 })
+
 loadRequest.done(function(dataFromServer) {
   var itemsData = dataFromServer.items
 
   itemsData.forEach(function(itemData) {
     addItemToPage(itemData)
   })
+})
+$('#add-form').on('submit', function(event) {
+  var itemDescription = event.target.itemDescription.value
+  event.preventDefault()
+  // alert('trying to create a new item with a description ' + itemDescription)
+  var creationRequest = $.ajax({
+  type: 'POST',
+  url: "http://listalous.herokuapp.com/lists/Wash-Day-Routine-Checklist/items",
+  data: { description: itemDescription, completed: false }
+})
+creationRequest.done(function(itemDataFromServer) {
+  addItemToPage(itemDataFromServer)
+})
 })
